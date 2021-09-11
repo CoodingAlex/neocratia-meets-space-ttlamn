a7ef775ac265e03bdf4c0802933ceb64f27ecf2f
CoodingAlex <coodingalex@gmail.com>
2021-09-11 14:06:48 -0600

    banner improved

---
+++ src/app/pages/rocket-svg/rocket-svg.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rocket-svg',
+  templateUrl: './rocket-svg.component.svg'
+})
+export class RocketSvgComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
