6c92a09096ad1ceded5ad6cf04fb92d6c57cc8a3
CoodingAlex <coodingalex@gmail.com>
2021-09-11 13:12:32 -0600

    start adding components

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -6,9 +6,11 @@ import { TeamComponent } from './team/team.component';
 import { ProductsComponent } from './products/products.component';
 import { NgMaterialModule } from '../core/ng-material.module';
 
+import { WhyComponent } from './why/why.component';
+
 @NgModule({
   imports: [CommonModule, PagesRoutingModule, NgMaterialModule],
-  declarations: [HomeComponent, TeamComponent, ProductsComponent],
+  declarations: [HomeComponent, TeamComponent, ProductsComponent, WhyComponent],
   exports: [ProductsComponent, HomeComponent, TeamComponent, ProductsComponent]
 })
 export class PagesModule {}
---
+++ src/app/pages/why/why.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-why',
+  templateUrl: './why.component.html'
+})
+export class WhyComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
